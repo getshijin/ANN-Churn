0a463200024fdade323cf830ec85d97adc1013ae
Shijin Mohanan Sheela <39332071+getshijin@users.noreply.github.com>
2025-01-22 22:31:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pickle
+import pandas as pd
+
+
+model = tf.keras.models.load_model('model.h5')
+
+### theencoder and scaler
+with open('c:\\ML\\ANN\\label_encoder_gender.pkl','rb') as file:
+    label_encoder_gender =pickle.load(file)
+
+with open('c:\\ML\\ANN\\onehot_encoder_geo.pkl','rb') as file:
+    onehot_encoder_geo =pickle.load(file)
+
+with open('c:\\ML\\ANN\\scaler.pkl','rb') as file:
+    scaler =pickle.load(file)
+
+#streamlet app
+st.title('Customer Churn Prediction')
+
+#streamlet app User Input
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0]) 
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Ages', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure',0,10)
+num_of_products = st.slider('Number of Products',1,4)
+has_cr_card = st.selectbox('Has Credit Card',[0,1])
+is_active_member = st.selectbox('is Activate Mamber',[0,1])
+
+#Prepare input data
+input_data = pd.DataFrame({
+    'CreditScore' : [credit_score],
+    'Gender' : [label_encoder_gender.transform([gender])[0]],
+    'Age' :[age],
+    'Tenure' : [tenure],
+    'Balance' : [balance],
+    'NumOfProducts' : [num_of_products],
+    'HasCrCard':[has_cr_card],
+    'IsActiveMember' : [is_active_member],
+    'EstimatedSalary' : [estimated_salary]
+})
+
+#one hot encoder 'Geograhy
+
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded,columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+#converting into DF
+input_data =pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
+
+#Scaled the input data
+input_data_scaled = scaler.transform(input_data)
+
+#Prediction churn
+prediction = model.predict(input_data_scaled)
+prediction_prob = prediction[0][0]
+
+if prediction_prob>.5:
+    st.write(f'the customer is likey to{prediction_prob}')
+    st.write('the customer is likey to Churn')
+else:
+    st.write('the customer is not likey to Churn')
+
